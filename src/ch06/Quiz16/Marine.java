ef156d9f47e93b993e2dd97ac6e2a66869f80485
Jjeongmean <exo1461@naver.com>
2024-02-16 18:16:00 +0900

    20240216

---
+++ src/ch06/Quiz16/Marine.java
@@ -0,0 +1,24 @@
+package ch06.Quiz16;
+
+public class Marine {
+    // 필드
+    int x, y = 0; // Marine의 위치좌표
+    int hp = 60; // 현재 체력
+    static int weapon = 6; // 공격력 , static을 붙이는 순간 공유한다
+    static int armor = 0; // 방어력
+
+    // 1. 공격력 증가시키는 메소드
+    static void weaponUp() {
+        weapon++;
+    }
+
+    // 2. 방어력 증가시키는 메소드
+    static void armorUp () { // 공유
+        armor++;
+    }
+    // 3. 캐릭터의 위치 움직이는 메소드
+    void move (int x, int y) {
+        this.x = x; // 개인
+        this.y = y;
+    }
+}
